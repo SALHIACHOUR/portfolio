a1ca08bb6db287526ee2a0e3074e2fc3eb76f321
SALHI Achour <salhiachour98@gmail.com>
2026-03-09 15:58:16 +0000

    clean code

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: 'export',
+    basePath: '/portfolio',   // nom exact de ton repo GitHub
+    assetPrefix: '/portfolio/',
+    images: { unoptimized: true}
   /* config options here */
 };
 
